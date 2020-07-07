cd21b90fc2808ef38cef5da4cb026bb921c89486
Ney Moura <neymourac@gmail.com>
2020-07-07 10:27:04 -0300

    Preparing project to be exported as a module

--- app.js
+++
@@ -1,7 +0,0 @@
-const multiSearchController = require('./controller/MultiSearchController');
-
-var args = process.argv.slice(2);
-
-multiSearchController.search(args[0], (content) => {
-    console.log(content)
-});
